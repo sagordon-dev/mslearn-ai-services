bfdce2dd79127b9ee9c99ee69788b9388afdd6ec
scott <scott.gordon72@outlook.com>
2024-03-27 15:29:21 -0400

    init commit

--- Labfiles/02-ai-services-security/rest-test.sh
+++ Labfiles/02-ai-services-security/rest-test.sh
@@ -1,1 +1,1 @@
-curl -X POST "<your-endpoint>/text/analytics/v3.1/languages?'" -H "Content-Type: application/json" -H "Ocp-Apim-Subscription-Key: <your-key>" --data-ascii "{'documents':[{'id':1,'text':'hello'}]}"
+curl -X POST "https://mslearn-ai-services-43047.cognitiveservices.azure.com/text/analytics/v3.1/languages?'" -H "Content-Type: application/json" -H "Ocp-Apim-Subscription-Key: 1cbc96f6b02d4390a053cab5488a8288" --data-ascii "{'documents':[{'id':1,'text':'hello'}]}"
